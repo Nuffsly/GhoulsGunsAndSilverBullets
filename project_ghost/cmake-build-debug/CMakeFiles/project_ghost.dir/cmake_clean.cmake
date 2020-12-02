6bffbf02f923e90abba1e95b7225918e48ea61ab
Jimte145 <jimte145@student.liu.se>
2020-12-02 11:22:56 +0100

    project_ghost initiated using CLion

---
+++ project_ghost/cmake-build-debug/CMakeFiles/project_ghost.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_ghost.dir/main.cpp.o"
+  "project_ghost"
+  "project_ghost.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_ghost.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
