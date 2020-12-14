531702f8597fc889b1cebf20fadcd6809f1249c3
Jimte145 <jimte145@student.liu.se>
2020-12-14 15:19:01 +0100

    Made upgrade texture. Added Sound_Manager.h

---
+++ project_ghost/managers/Sound_Manager.cpp
@@ -0,0 +1,5 @@
+//
+// Created by jimte on 2020-12-14.
+//
+
+#include "Sound_Manager.h"
---
+++ project_ghost/managers/Sound_Manager.h
@@ -0,0 +1,15 @@
+//
+// Created by jimte on 2020-12-14.
+//
+
+#ifndef MAIN_CPP_SOUND_MANAGER_H
+#define MAIN_CPP_SOUND_MANAGER_H
+
+
+class Sound_Manager
+{
+
+};
+
+
+#endif //MAIN_CPP_SOUND_MANAGER_H
