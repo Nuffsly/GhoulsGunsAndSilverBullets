8fc878ea7e83113b2221ed9d39c4a9160ff2741d
Jimte145 <jimte145@student.liu.se>
2020-12-14 17:51:39 +0100

    Added looping background music

--- project_ghost/main.cpp
+++ project_ghost/main.cpp
@@ -1,4 +1,5 @@
 #include<SFML/Graphics.hpp>
+#include <SFML/Audio.hpp>
 
 #include "states/State.h"
 #include "states/Game_State.h"
@@ -10,6 +11,16 @@ int main()
     sf::RenderWindow window{sf::VideoMode(1280,720), "PROJECT: [G H 0 5 T]", sf::Style::Titlebar | sf::Style::Close};
     window.setVerticalSyncEnabled(true);
     window.setKeyRepeatEnabled(false);
+
+    //Start playing BG music
+    sf::Music music{};
+    if(!music.openFromFile("../audio_data/background_music.wav"))
+    {
+        throw std::logic_error("Failed to load background music.");
+    }
+    music.setLoop(true);
+    music.setVolume(25);
+    music.play();
 
     State::run(window, std::make_shared<Game_State>(window));
 
