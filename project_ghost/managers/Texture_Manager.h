b69c7c08df6a56fd207dd6b48fd1d7873fa0d72e
Jimte145 <jimte145@student.liu.se>
2020-12-15 09:51:14 +0100

    Added Font_Manager in Texture_Manager. Also font pixel.ttf

--- project_ghost/managers/Texture_Manager.h
+++ project_ghost/managers/Texture_Manager.h
@@ -10,15 +10,28 @@
 class Texture_Manager
 {
 public:
-    static sf::Texture* get_texture(std::string const& name);
+    static sf::Texture *get_texture(std::string const &name);
 
 private:
     Texture_Manager() = default;
 
     static Texture_Manager texture_storage;
 
-   std::map<std::string, std::unique_ptr<sf::Texture>> textures;
+    std::map<std::string, std::unique_ptr<sf::Texture>> textures;
 
+};
+
+class Font_Manager
+{
+public:
+    static sf::Font& get_font(std::string const& name);
+
+private:
+    Font_Manager() = default;
+
+    static Font_Manager font_storage;
+
+    std::map<std::string, std::unique_ptr<sf::Font>> fonts;
 };
 
 
