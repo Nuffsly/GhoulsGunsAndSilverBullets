45f9edf00854e673b17725e8cbe3ef9c8b235329
Martin Kuiper <marku849@student.liu.se>
2020-12-16 18:04:37 +0100

    Added capture of mouse in game window

--- project_ghost/main.cpp
+++ project_ghost/main.cpp
@@ -9,8 +9,7 @@ int main()
     sf::RenderWindow window{sf::VideoMode(1280,720), "PROJECT: [G H 0 5 T]", sf::Style::Titlebar | sf::Style::Close};
     window.setVerticalSyncEnabled(true);
     window.setKeyRepeatEnabled(false);
-    //window.setMouseCursorGrabbed(true);
-    //window.setMouseCursorVisible(false);
+    window.setMouseCursorGrabbed(true);
 
     State::run(window, std::make_shared<Menu_State>(window));
 
