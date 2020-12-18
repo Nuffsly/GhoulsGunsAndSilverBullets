ba546267df9f2b81604eb85fdfb06fd1bfbdde2a
Jimte145 <jimte145@student.liu.se>
2020-12-18 19:01:01 +0100

    Added cmake-build-debug and wrote Readme file

---
+++ project_ghost/cmake-build-debug/CMakeFiles/project_ghost.dir/cmake_clean.cmake
@@ -0,0 +1,28 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/project_ghost.dir/containers/Player_Info.cpp.o"
+  "CMakeFiles/project_ghost.dir/containers/Upgrade.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Door.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Enemy.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Game_Object.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Hud.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Money.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Player.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Projectile.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Textured_Object.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Upgrade_Pillar.cpp.o"
+  "CMakeFiles/project_ghost.dir/game_objects/Weapon.cpp.o"
+  "CMakeFiles/project_ghost.dir/main.cpp.o"
+  "CMakeFiles/project_ghost.dir/managers/Sound_Manager.cpp.o"
+  "CMakeFiles/project_ghost.dir/managers/Texture_Manager.cpp.o"
+  "CMakeFiles/project_ghost.dir/managers/World.cpp.o"
+  "CMakeFiles/project_ghost.dir/states/Game_State.cpp.o"
+  "CMakeFiles/project_ghost.dir/states/Menu_State.cpp.o"
+  "CMakeFiles/project_ghost.dir/states/State.cpp.o"
+  "project_ghost"
+  "project_ghost.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/project_ghost.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
